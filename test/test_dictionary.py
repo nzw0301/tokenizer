cb654b53e6bf3da9062202d2e3a690f097cb6981
Kento Nozawa <k_nzw@klis.tsukuba.ac.jp>
2019-04-16 15:46:37 +0900

    Fix styles

--- test/test_dictionary.py
+++ test/test_dictionary.py
@@ -3,12 +3,12 @@ import numpy as np
 import os
 import string
 
-TEXT8_LIKE_FILE_NAME = 'text8-like.txt'
-DOC_FILE_NAME = 'doc.txt'
+TEXT8_LIKE_FILE_NAME = "text8-like.txt"
+DOC_FILE_NAME = "doc.txt"
 
 
 def create_test_corpus_files():
-    with open(TEXT8_LIKE_FILE_NAME, 'w') as f:
+    with open(TEXT8_LIKE_FILE_NAME, "w") as f:
         """
         file content: " a b b c c c ... z"
         """
@@ -16,9 +16,9 @@ def create_test_corpus_files():
         for count, char in enumerate(string.ascii_lowercase, start=1):
             for _ in range(count):
                 words.append(char)
-        f.write(' '.join(words))
+        f.write(" ".join(words))
 
-    with open(DOC_FILE_NAME, 'w') as f:
+    with open(DOC_FILE_NAME, "w") as f:
         """
         file content:
 
@@ -37,9 +37,9 @@ def create_test_corpus_files():
             words = []
             for _ in range(count):
                 words.append(char)
-            f.write('\n')
-            f.write(' '.join(words) + '\n')
-        f.write('\n')
+            f.write("\n")
+            f.write(" ".join(words) + "\n")
+        f.write("\n")
 
 
 def delete_test_corpus_files():
@@ -50,40 +50,40 @@ def delete_test_corpus_files():
 def tests():
     def test_fit_from_fname(fname):
         for in_memory in [False, True]:
-            dictionary = Dictionary(min_count=5, replace_lower_freq_word=False, replace_word='')
+            dictionary = Dictionary(min_count=5, replace_lower_freq_word=False, replace_word="")
             dictionary.fit_from_fname(fname, in_memory=in_memory)
             assert len(dictionary.vocab) == 26-4
-            assert dictionary.vocab.id2word[0] == 'z'
-            assert dictionary.vocab.id2word[-1] == 'e'
+            assert dictionary.vocab.id2word[0] == "z"
+            assert dictionary.vocab.id2word[-1] == "e"
             assert dictionary.vocab.id2freq[0] == 26
             assert dictionary.num_words == np.sum(np.arange(5, 27))
 
     def test_fit_from_docs(docs):
-        dictionary = Dictionary(min_count=5, replace_lower_freq_word=False, replace_word='')
+        dictionary = Dictionary(min_count=5, replace_lower_freq_word=False, replace_word="")
         dictionary.fit(docs)
         assert len(dictionary.vocab) == 26-4
-        assert dictionary.vocab.id2word[0] == 'z'
-        assert dictionary.vocab.id2word[-1] == 'e'
+        assert dictionary.vocab.id2word[0] == "z"
+        assert dictionary.vocab.id2word[-1] == "e"
         assert dictionary.vocab.id2freq[0] == 26
         assert dictionary.num_words == np.sum(np.arange(5, 27))
 
     def test_fit_min_count1(docs):
-        dictionary = Dictionary(min_count=1, replace_lower_freq_word=False, replace_word='')
+        dictionary = Dictionary(min_count=1, replace_lower_freq_word=False, replace_word="")
         dictionary.fit(docs)
         assert len(dictionary.vocab) == 26
-        assert dictionary.vocab.id2word[0] == 'z'
-        assert dictionary.vocab.word2id['z'] == 0
-        assert dictionary.vocab.id2word[-1] == 'a'
+        assert dictionary.vocab.id2word[0] == "z"
+        assert dictionary.vocab.word2id["z"] == 0
+        assert dictionary.vocab.id2word[-1] == "a"
         assert dictionary.vocab.id2freq[0] == 26
         assert dictionary.num_words == np.sum(np.arange(1, 27))
 
     def test_fit_with_replace_and_add_special_from_fname(fname):
-        dictionary = Dictionary(min_count=5, replace_lower_freq_word=True, replace_word='<UNK>')
+        dictionary = Dictionary(min_count=5, replace_lower_freq_word=True, replace_word="<UNK>")
         dictionary.fit(fname)
 
-        assert dictionary.vocab.id2word[-1] == '<UNK>'
+        assert dictionary.vocab.id2word[-1] == "<UNK>"
         assert dictionary.vocab.id2freq[0] == 26
-        assert dictionary.vocab.id2word[0] == 'z'
+        assert dictionary.vocab.id2word[0] == "z"
         assert len(dictionary.vocab) == 26 - 4 + 1
         assert dictionary.num_vocab == 26 - 4 + 1
         assert dictionary.num_words == np.sum(np.arange(27))
@@ -108,7 +108,7 @@ def tests():
         test_fit_from_fname(fname=fname)
         test_fit_with_replace_and_add_special_from_fname(fname=fname)
 
-        if 'text8' in fname:
+        if "text8" in fname:
             continue
 
         with open(fname) as f:
