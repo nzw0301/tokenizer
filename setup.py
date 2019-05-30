908598d23f7fbf5125439b4d7e06dbe9ba6dd40e
NOZAWA Kento <k_nzw@klis.tsukuba.ac.jp>
2019-05-30 14:53:19 +0900

    Update version

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name="tokenizer",
-      version="0.0.1",
+      version="0.1.1",
       description="Utils for nlp",
       packages=find_packages())
